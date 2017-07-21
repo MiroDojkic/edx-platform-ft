67870b0a9190f546b07dc9fe354bc2831a3a3d36
Marin Bareta <mbareta@extensionengine.com>
2017-07-21 10:38:11 +0200

    Post init

--- lms/djangoapps/affiliates/models.py
+++ lms/djangoapps/affiliates/models.py
@@ -4,11 +4,15 @@ from lms.envs.common import STATE_CHOICES
 from django_countries.fields import CountryField
 
 
+def user_directory_path(instance, filename):
+    return 'user_{0}/{1}'.format(instance.id, filename)
+
+
 class AffiliateEntity(models.Model):
     email = models.CharField(max_length=255)
     name = models.CharField(max_length=255)
     description = models.CharField(null=True, blank=True, max_length=255)
-    phone_number = models.CharField(null=True, blank=True, default='', max_length=255)
+    phone_number = models.CharField(null=True, blank=True, max_length=255)
     address = models.CharField(null=True, blank=True, max_length=255)
     city = models.CharField(null=True, blank=True, max_length=255)
     zipcode = models.CharField(null=True, blank=True, max_length=255)
@@ -17,6 +21,12 @@ class AffiliateEntity(models.Model):
     linkedin = models.CharField(null=True, blank=True, max_length=255)
     state = models.CharField(null=True, blank=True, default='na', choices=STATE_CHOICES, max_length=255)
     country = CountryField(blank=True, null=True)
+
+    image = models.ImageField(upload_to=user_directory_path)
+    members = models.ManyToManyField(User, null=True, through='AffiliateMembership')
+
+    def __unicode__(self):
+        return self.name
 
 
 class AffiliateMembership(models.Model):
@@ -28,4 +38,4 @@ class AffiliateMembership(models.Model):
 
     member = models.ForeignKey(User)
     affiliate = models.ForeignKey(AffiliateEntity)
-    role = models.CharField(choices=STATE_CHOICES, max_length=255)
+    role = models.CharField(choices=role_choices, max_length=255)
