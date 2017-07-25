7823e387fa96b7828e541ede92b3944d4cb05206
Miro Dojkic <mdojkic@gmail.com>
2017-07-25 08:52:38 +0200

    filter by user id in query

--- lms/djangoapps/ccx/views.py
+++ lms/djangoapps/ccx/views.py
@@ -154,12 +154,11 @@ def edit_ccx_context(course, ccx, user):
     context['save_url'] = reverse(
         'save_ccx', kwargs={'course_id': ccx_locator})
 
-    ccx_members = CourseEnrollment.objects.filter(course_id=ccx_locator, is_active=True)
     non_student_user_ids = CourseAccessRole.objects.filter(course_id=ccx_locator).values_list('user_id', flat=True)
+    ccx_student_enrollments = CourseEnrollment.objects.filter(course_id=ccx_locator, is_active=True).exclude(user_id__in=non_student_user_ids)
 
-    only_students = lambda member: not member.user.id in non_student_user_ids
 
-    context['ccx_students'] = filter(only_students, ccx_members)
+    context['ccx_students'] = ccx_student_enrollments
     context['gradebook_url'] = reverse(
         'ccx_gradebook', kwargs={'course_id': ccx_locator})
     context['grades_csv_url'] = reverse(
@@ -217,12 +216,11 @@ def dashboard(request, course, ccx=None):
         context['ccx_courses'] = custom_courses
         context['edit_current'] = False
 
-        ccx_members = CourseEnrollment.objects.filter(course_id=ccx_locator)
         non_student_user_ids = CourseAccessRole.objects.filter(course_id=ccx_locator).values_list('user_id', flat=True)
-        only_students = lambda member: not member.user.id in non_student_user_ids
+        ccx_student_enrollments = CourseEnrollment.objects.filter(course_id=ccx_locator).exclude(user_id__in=non_student_user_ids)
 
         # show students on Student Admin tab
-        context['enrollments'] = filter(only_students, ccx_members)
+        context['enrollments'] = ccx_student_enrollments
     else:
         context['create_ccx_url'] = reverse(
             'create_ccx', kwargs={'course_id': course.id})
