4940e01c7f81747672ab7a4bae99d735f6386330
Miro Dojkic <mdojkic@gmail.com>
2017-09-01 15:23:33 +0200

    calculate distance by great circle method

--- lms/djangoapps/courseware/gis_helpers.py
+++ lms/djangoapps/courseware/gis_helpers.py
@@ -7,6 +7,7 @@ def change_in_latitude(miles):
     Given a distance north, return the change in latitude.
     """
     return math.degrees(miles/EARTH_RADIUS)
+
 def change_in_longitude(latitude, miles):
     """
     Given a latitude and a distance west, return the change in longitude.
@@ -14,3 +15,23 @@ def change_in_longitude(latitude, miles):
     """
     r = EARTH_RADIUS*math.cos(math.radians(latitude))
     return math.degrees(miles/r)
+
+def coordinates_distance(first, second):
+    latitude_1, longitude_1 = math.radians(
+        first['latitude']), math.radians(first['longitude'])
+    latitude_2, longitude_2 = math.radians(
+        second['latitude']), math.radians(second['longitude'])
+
+    sin_latitude_1, cos_latitude_1 = math.sin(latitude_1), math.cos(latitude_1)
+    sin_latitude_2, cos_latitude_2 = math.sin(latitude_2), math.cos(latitude_2)
+
+    delta_longitude = longitude_2 - longitude_1
+    cos_delta_longitude, sin_delta_longitude = math.cos(
+        delta_longitude), math.sin(delta_longitude)
+
+    distance = math.atan2(math.sqrt((cos_latitude_2 * sin_delta_longitude) ** 2 +
+                             (cos_latitude_1 * sin_latitude_2 -
+                              sin_latitude_1 * cos_latitude_2 * cos_delta_longitude) ** 2),
+                   sin_latitude_1 * sin_latitude_2 + cos_latitude_1 * cos_latitude_2 * cos_delta_longitude)
+
+    return EARTH_RADIUS * distance
