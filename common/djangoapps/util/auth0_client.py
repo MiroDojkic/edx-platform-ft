4e0135b2698ea5cca6244a66d0b9a0a3b3e04235
Miro Dojkic <mdojkic@gmail.com>
2017-10-11 16:22:34 +0200

    init

---
+++ common/djangoapps/util/auth0_client.py
@@ -0,0 +1,14 @@
+class Auth0ManagementClient(object):
+    __url = None
+    __token = None
+    __ttl = 1000 * 60 * 20 # 20 minutes retention
+
+    def __init__(self, url):
+        self.__url = url
+
+    def __refresh_token__():
+      print 'Refreshing token...'
+
+    def create_user():
+      print 'Creating user...'
+
